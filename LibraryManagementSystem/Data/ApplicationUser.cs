6bf50542f79dd3f7922ed2cf176bd779ca6895e6
tsctsc6 <tsctsc6@126.com>
2025-07-10 19:25:02 +0800

    添加额外字段

--- LibraryManagementSystem/Data/ApplicationUser.cs
+++ LibraryManagementSystem/Data/ApplicationUser.cs
@@ -1,3 +1,4 @@
+using System.ComponentModel.DataAnnotations;
 using Microsoft.AspNetCore.Identity;
 
 namespace LibraryManagementSystem.Data;
@@ -5,4 +6,6 @@ namespace LibraryManagementSystem.Data;
 // Add profile data for application users by adding properties to the ApplicationUser class
 public class ApplicationUser : IdentityUser
 {
+    [Required]
+    public bool IsPasswordChanged { get; set; } = false;
 }
